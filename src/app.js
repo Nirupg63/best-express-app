7cf3aac0fce743f2d4310ecbc5180d60e13188ec
nmuralidharan <nmuralidharan@aienterprise.com>
2020-08-18 15:21:09 +0530

    added env variables

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-
 const app = express()
-const port = 3000
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,19 +11,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb://localhost:27017/people', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(`mongodb://${process.env.DB_HOST}:27017/people`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("We're connected to mongo")
 });
 
-app.get('/', (req, res) => {
-  res.send('Testing')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.APP_PORT}`)
 })
 
 const personSchema = new mongoose.Schema({
@@ -31,6 +31,12 @@ const personSchema = new mongoose.Schema({
   name: String
 });
 const Person = mongoose.model("Person", personSchema)
+
+
+
+app.get('/', (req, res) => {
+  res.send('Testing')
+})
 
 app.get('/person/:id', (req, res) => {
   Person.find({id: req.params.id}, (err, doc) => {
